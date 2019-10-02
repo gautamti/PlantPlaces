ad97d77cf8eb5e31430e94eaee18b298e06358fd
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-01 21:12:27 -0400

    Commit changes

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,12 +1,34 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantPlacesController {
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"royalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"royalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
 		return "start";
 	}
 	
